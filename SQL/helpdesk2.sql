a722a3d863104b224f309612f48f88d61d886c03
Leandro Marim <leandro.marim@hotmail.com>
2019-05-16 02:32:33 -0300

    Update das tabelas

--- SQL/helpdesk2.sql
+++ SQL/helpdesk2.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: helpdesk2
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	8.0.13
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -40,7 +40,7 @@ CREATE TABLE `chamados` (
   CONSTRAINT `fk_prioridade` FOREIGN KEY (`id_prioridade`) REFERENCES `prioridade` (`id`),
   CONSTRAINT `fk_problema` FOREIGN KEY (`id_problema`) REFERENCES `tbl_problema` (`id_problema`),
   CONSTRAINT `fk_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,7 +49,7 @@ CREATE TABLE `chamados` (
 
 LOCK TABLES `chamados` WRITE;
 /*!40000 ALTER TABLE `chamados` DISABLE KEYS */;
-INSERT INTO `chamados` VALUES (1,3,'2019-05-14 10:18:24','cliente','Adm','teste',2,'Pendente',NULL,2);
+INSERT INTO `chamados` VALUES (1,3,'2019-05-14 10:18:24','cliente','Adm','Estou com problemas ao acessar o site globo.com',2,'Pendente',NULL,3),(2,1,'2019-05-15 21:38:23','administrativo','master','Gostaria de visualizar o site do UOL mas nada está aparecendo',2,'Pendente',NULL,2),(3,3,'2019-05-15 21:39:14','cliente','Adm','Irei mudar para duas ruas acima da minha e queria fazer a transferência',4,'Pendente',NULL,1),(5,1,'2019-05-16 01:56:05','administrativo','master','Quero ver minhas novelas, não aguento mais',2,'Resolvido','2019-05-16 02:12:00',2);
 /*!40000 ALTER TABLE `chamados` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +65,7 @@ CREATE TABLE `confirma` (
   `idconfirma` int(11) NOT NULL AUTO_INCREMENT,
   `idusuario` int(11) NOT NULL,
   PRIMARY KEY (`idconfirma`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,7 +89,7 @@ CREATE TABLE `prioridade` (
   `prioridade_desc` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `sla` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +98,7 @@ CREATE TABLE `prioridade` (
 
 LOCK TABLES `prioridade` WRITE;
 /*!40000 ALTER TABLE `prioridade` DISABLE KEYS */;
-INSERT INTO `prioridade` VALUES (1,'Critico',12),(2,'Urgente',24),(3,'Intermediário',72),(4,'Básico',96);
+INSERT INTO `prioridade` VALUES (1,'Crítica',12),(2,'Alta',24),(3,'Média',72),(4,'Baixa',96);
 /*!40000 ALTER TABLE `prioridade` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,12 +114,13 @@ CREATE TABLE `respostas` (
   `resposta` varchar(1000) NOT NULL,
   `idusuario` int(11) NOT NULL,
   `idchamado` int(11) NOT NULL,
+  `anexo` varchar(100) DEFAULT NULL,
   `data` varchar(10) DEFAULT NULL,
   `hora` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idusuario` (`idusuario`),
   CONSTRAINT `respostas_ibfk_1` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +129,7 @@ CREATE TABLE `respostas` (
 
 LOCK TABLES `respostas` WRITE;
 /*!40000 ALTER TABLE `respostas` DISABLE KEYS */;
-INSERT INTO `respostas` VALUES (1,'teste',19,66,'14/05/2019','10:19:49'),(2,'teste12',19,66,'14/05/2019','10:19:56');
+INSERT INTO `respostas` VALUES (24,'teste',1,3,'M18xX0NfX2NoYW1hZG9zLnhtbA==','16/05/2019','04:28:38'),(25,'oi',1,3,'M18xX0NfX2NoYW1hZG9zLnhtbA==','16/05/2019','04:35:51');
 /*!40000 ALTER TABLE `respostas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +144,7 @@ CREATE TABLE `tbl_problema` (
   `id_problema` int(11) NOT NULL AUTO_INCREMENT,
   `desc_problema` varchar(40) NOT NULL,
   PRIMARY KEY (`id_problema`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,7 +153,7 @@ CREATE TABLE `tbl_problema` (
 
 LOCK TABLES `tbl_problema` WRITE;
 /*!40000 ALTER TABLE `tbl_problema` DISABLE KEYS */;
-INSERT INTO `tbl_problema` VALUES (1,'Internet lenta'),(2,'Abrir site especifico'),(3,'Não abre site nenhum'),(4,'Mudança de enreço'),(5,'Mudança do local do roteado'),(6,'Outros');
+INSERT INTO `tbl_problema` VALUES (1,'Internet lenta/Sem conexão'),(2,'Não consigo abrir um site específico'),(3,'Não consigo navegar em nenhum site'),(4,'Mudança de endereço'),(5,'Troca do roteador'),(6,'Outros');
 /*!40000 ALTER TABLE `tbl_problema` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,7 +178,7 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`idusuario`),
   UNIQUE KEY `login` (`login`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -186,7 +187,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'master','','master','202cb962ac59075b964b07152d234b70','administrativo','2017-07-17 01:46:06','ativo','0000-00-00 00:00:00',0),(2,'Severino','severino@hotmail.com','severino','202cb962ac59075b964b07152d234b70','RH','2019-03-30 17:27:06','ativo','0000-00-00 00:00:00',0),(3,'Adm','leandro.marim@hotmail.com','marim48','e7d80ffeefa212b7c5c55700e4f7193e','cliente','2019-05-14 10:15:56','ativo','0000-00-00 00:00:00',0);
+INSERT INTO `usuario` VALUES (1,'master','','master','202cb962ac59075b964b07152d234b70','administrativo','2017-07-17 01:46:06','ativo','0000-00-00 00:00:00',0),(3,'Adm','leandro.marim@hotmail.com','marim48','e7d80ffeefa212b7c5c55700e4f7193e','cliente','2019-05-14 10:15:56','ativo','0000-00-00 00:00:00',0);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -199,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-14 10:22:56
+-- Dump completed on 2019-05-16  2:27:53
